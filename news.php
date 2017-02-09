08639c0df369f65ab68f211b316c9df720c5a711
Sergey731 <psv.731@yandex.ru>
2017-02-09 18:35:36 +0500

    news.php improved

--- news.php
+++ news.php
@@ -1,21 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Яндекс-новости</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
 </head>
 <body>
-    <?php
-    $monthes = [
-        1 => 'января', 2 => 'февраля', 3 => 'марта', 4 => 'апреля',
-        5 => 'мая', 6 => 'июня', 7 => 'июля', 8 => 'августа',
-        9 => 'сентября', 10 => 'октября', 11 => 'ноября', 12 => 'декабря'
+    <?php    
+    $months = [
+        'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'
     ];
-    $news_date = (date('Новости за d ') . $monthes[(date('n'))] . date(' Y года: '));
+    $news_date = date('Новости за d ') . $months[(date('n') - 1)] . date(' Y года: ');
     echo "<h3>$news_date</h3>";    
     
-    $page = file_get_contents("https://www.yandex.ru");
+    $page = file_get_contents('https://www.yandex.ru');
     $marker = '"link list__item-content link_black_yes" aria-label="';
     $marker_count = substr_count($page, $marker);
     $pos = strpos($page, $marker);
